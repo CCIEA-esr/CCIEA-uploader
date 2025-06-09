1c0e01f98a17d2264984cc549a980378c88dc2de
lmdewitt <lynn.dewitt@noaa.gov>
2025-06-09 12:24:48 -0700

    update GitHub actions R code to include file naming conventions

--- get_status.R
+++ get_status.R
@@ -1,10 +1,11 @@
 ## GitHub action to look at Google Drive and update uploader status, runs daily
-source("_init.R")
+## valuess of function parameters are in _init.R (sourced in _pi_folders_to_json.R )
 source("_gdrive.R")
 source("_pi_folders_to_json.R")
 generate_file_status(esr_year,headervars,headervarsmon)
 get_indices(esr_year,last_year)
 last_updated <- read_updated(esr_year)
+get_file_conventions(cciea_folders[2],file_name_conventions)
 print(last_updated)
 
 
